3835b4ac81e841929de1d7287e134e86ea49d9f6
SlashNephy <slashnephy@hatena.ne.jp>
2025-10-22 13:45:11 +0900

    ImageLoader を provide する

--- core/ui/src/main/java/blue/starry/mitsubachi/ui/di/RelativeDateTimeFormatterModule.kt
+++ core/ui/src/main/java/blue/starry/mitsubachi/ui/di/RelativeDateTimeFormatterModule.kt
@@ -4,13 +4,23 @@ import blue.starry.mitsubachi.ui.formatter.RelativeDateTimeFormatter
 import blue.starry.mitsubachi.ui.formatter.RelativeDateTimeFormatterImpl
 import dagger.Binds
 import dagger.Module
+import dagger.Provides
 import dagger.hilt.InstallIn
 import dagger.hilt.components.SingletonComponent
 import javax.inject.Singleton
+import android.icu.text.RelativeDateTimeFormatter as AndroidRelativeDateTimeFormatter
 
 @Module
 @InstallIn(SingletonComponent::class)
 abstract class RelativeDateTimeFormatterModule {
+  companion object {
+    @Provides
+    @Singleton
+    fun provideRelativeDateTimeFormatter(): AndroidRelativeDateTimeFormatter {
+      return AndroidRelativeDateTimeFormatter.getInstance()
+    }
+  }
+
   @Binds
   @Singleton
   abstract fun bind(impl: RelativeDateTimeFormatterImpl): RelativeDateTimeFormatter
