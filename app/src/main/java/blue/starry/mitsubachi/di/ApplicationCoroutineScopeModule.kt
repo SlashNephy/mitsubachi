3cc25b850f13d9f333487453d40aa88f65d0c732
SlashNephy <slashnephy@hatena.ne.jp>
2025-11-03 21:13:56 +0900

    ApplicationScope でスコープを注入できるようにする

---
+++ app/src/main/java/blue/starry/mitsubachi/di/ApplicationCoroutineScopeModule.kt
@@ -0,0 +1,22 @@
+package blue.starry.mitsubachi.di
+
+import blue.starry.mitsubachi.domain.ApplicationScope
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.SupervisorJob
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+internal object ApplicationCoroutineScopeModule {
+  @Singleton
+  @Provides
+  @ApplicationScope
+  fun provide(): CoroutineScope {
+    return CoroutineScope(SupervisorJob() + Dispatchers.Default)
+  }
+}
