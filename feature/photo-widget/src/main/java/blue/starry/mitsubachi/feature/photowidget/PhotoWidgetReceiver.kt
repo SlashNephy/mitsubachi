972743c174dfa85bc71ef0c9a1dd7ff5acb9a08d
SlashNephy <spica@starry.blue>
2025-11-11 21:10:56 +0900

    onUpdate でもトリガーする

--- feature/photo-widget/src/main/java/blue/starry/mitsubachi/feature/photowidget/PhotoWidgetReceiver.kt
+++ feature/photo-widget/src/main/java/blue/starry/mitsubachi/feature/photowidget/PhotoWidgetReceiver.kt
@@ -1,5 +1,6 @@
 package blue.starry.mitsubachi.feature.photowidget
 
+import android.appwidget.AppWidgetManager
 import android.content.Context
 import androidx.glance.appwidget.GlanceAppWidget
 import androidx.glance.appwidget.GlanceAppWidgetReceiver
@@ -17,6 +18,15 @@ class PhotoWidgetReceiver : GlanceAppWidgetReceiver() {
 
   override fun onEnabled(context: Context) {
     super.onEnabled(context)
+    workerScheduler.enqueue()
+  }
+
+  override fun onUpdate(
+    context: Context,
+    appWidgetManager: AppWidgetManager,
+    appWidgetIds: IntArray,
+  ) {
+    super.onUpdate(context, appWidgetManager, appWidgetIds)
     workerScheduler.enqueue()
   }
 
