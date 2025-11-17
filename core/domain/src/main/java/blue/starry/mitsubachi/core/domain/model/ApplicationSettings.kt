2348bd663ab115942e69093de2232ddf1d971567
SlashNephy <spica@starry.blue>
2025-11-18 00:58:27 +0900

    mockk を Preview で使わなくて済むようにする

--- core/domain/src/main/java/blue/starry/mitsubachi/core/domain/model/ApplicationSettings.kt
+++ core/domain/src/main/java/blue/starry/mitsubachi/core/domain/model/ApplicationSettings.kt
@@ -2,4 +2,10 @@ package blue.starry.mitsubachi.core.domain.model
 
 data class ApplicationSettings(
   val isFirebaseCrashlyticsEnabled: Boolean,
-)
+) {
+  companion object {
+    val Default = ApplicationSettings(
+      isFirebaseCrashlyticsEnabled = true,
+    )
+  }
+}
--- feature/settings/src/main/java/blue/starry/mitsubachi/feature/settings/ui/SettingsScreen.kt
+++ feature/settings/src/main/java/blue/starry/mitsubachi/feature/settings/ui/SettingsScreen.kt
@@ -1,32 +1,10 @@
 package blue.starry.mitsubachi.feature.settings.ui
 
-import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.rememberScrollState
-import androidx.compose.foundation.verticalScroll
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.automirrored.filled.Logout
-import androidx.compose.material.icons.filled.BugReport
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Switch
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.compose.ui.unit.dp
 import androidx.hilt.lifecycle.viewmodel.compose.hiltViewModel
 import androidx.lifecycle.compose.collectAsStateWithLifecycle
-import blue.starry.mitsubachi.core.domain.model.ApplicationSettings
 import blue.starry.mitsubachi.core.ui.compose.screen.LoadingScreen
-import blue.starry.mitsubachi.core.ui.compose.setting.SettingItem
-import blue.starry.mitsubachi.core.ui.compose.setting.SettingSection
-import io.mockk.every
-import io.mockk.mockk
-import kotlinx.coroutines.flow.MutableStateFlow
-import kotlinx.coroutines.flow.asStateFlow
 
 @Composable
 fun SettingsScreen(
@@ -41,88 +19,17 @@ fun SettingsScreen(
     }
 
     is SettingsScreenViewModel.UiState.Loaded -> {
-      Surface(modifier = Modifier.fillMaxSize()) {
-        Column(
-          modifier = Modifier
-            .padding(24.dp)
-            .verticalScroll(rememberScrollState()),
-          verticalArrangement = Arrangement.spacedBy(16.dp),
-        ) {
-          AccountSection(
-            onClickLogout = {
-              viewModel.signOut().invokeOnCompletion { onSignOut() }
-            },
-          )
-
-          DataCollectionSection(
-            settings = state.applicationSettings,
-            onChangeIsFirebaseCrashlyticsEnabled = {
-              viewModel.update { settings ->
-                settings.copy(
-                  isFirebaseCrashlyticsEnabled = it,
-                )
-              }
-            },
-          )
-        }
-      }
+      SettingsContent(
+        applicationSettings = state.applicationSettings,
+        onChangeApplicationSettings = { block ->
+          viewModel.update { settings ->
+            settings.let(block)
+          }
+        },
+        onSignOut = {
+          viewModel.signOut().invokeOnCompletion { onSignOut() }
+        },
+      )
     }
   }
-}
-
-@Composable
-private fun AccountSection(onClickLogout: () -> Unit) {
-  SettingSection(title = "アカウント") {
-    item(
-      leadingIcon = SettingItem.LeadingIcon.Flat(Icons.AutoMirrored.Filled.Logout),
-      headlineText = "ログアウト",
-      modifier = Modifier.clickable(onClick = onClickLogout),
-    )
-  }
-}
-
-@Composable
-private fun DataCollectionSection(
-  settings: ApplicationSettings,
-  onChangeIsFirebaseCrashlyticsEnabled: (Boolean) -> Unit,
-) {
-  SettingSection(title = "データ収集") {
-    item(
-      leadingIcon = SettingItem.LeadingIcon.Flat(Icons.Default.BugReport),
-      headlineText = "バグレポートを自動的に送信する",
-      supportingText = "アプリがクラッシュした場合にエラーを Firebase Crashlytics に報告します。送信されるデータは匿名です。",
-      trailingContent = {
-        Switch(
-          checked = settings.isFirebaseCrashlyticsEnabled,
-          onCheckedChange = onChangeIsFirebaseCrashlyticsEnabled,
-        )
-      },
-    )
-  }
-}
-
-@Preview
-@Composable
-private fun SettingsScreenLoadingPreview() {
-  SettingsScreen(
-    onSignOut = {},
-    viewModel = mockk {
-      every { state } returns MutableStateFlow(SettingsScreenViewModel.UiState.Loading).asStateFlow()
-    },
-  )
-}
-
-@Preview
-@Composable
-private fun SettingsScreenLoadedPreview() {
-  SettingsScreen(
-    onSignOut = {},
-    viewModel = mockk {
-      every { state } returns MutableStateFlow(
-        SettingsScreenViewModel.UiState.Loaded(
-          applicationSettings = mockk(relaxed = true),
-        ),
-      ).asStateFlow()
-    },
-  )
 }
