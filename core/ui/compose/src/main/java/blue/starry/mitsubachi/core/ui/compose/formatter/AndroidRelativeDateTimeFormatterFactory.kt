3bbb8e80ef20d63045f73ee14750ddb9628d0104
SlashNephy <spica@starry.blue>
2025-11-24 22:35:27 +0900

    ゴミ削除

--- core/ui/compose/src/main/java/blue/starry/mitsubachi/core/ui/compose/formatter/AndroidRelativeDateTimeFormatterFactory.kt
+++
@@ -1,7 +0,0 @@
-package blue.starry.mitsubachi.core.ui.compose.formatter
-
-import android.icu.text.RelativeDateTimeFormatter as AndroidRelativeDateTimeFormatter
-
-fun interface AndroidRelativeDateTimeFormatterFactory {
-  fun create(): AndroidRelativeDateTimeFormatter
-}
