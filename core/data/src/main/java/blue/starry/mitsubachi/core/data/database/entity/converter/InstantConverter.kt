45bff862c63d06a272d19d49436bc0376dad5155
SlashNephy <spica@starry.blue>
2025-11-26 10:12:49 +0900

    余計なアノテーション

--- core/data/src/main/java/blue/starry/mitsubachi/core/data/database/entity/converter/InstantConverter.kt
+++ core/data/src/main/java/blue/starry/mitsubachi/core/data/database/entity/converter/InstantConverter.kt
@@ -2,9 +2,7 @@ package blue.starry.mitsubachi.core.data.database.entity.converter
 
 import androidx.room.TypeConverter
 import java.time.Instant
-import kotlin.time.ExperimentalTime
 
-@OptIn(ExperimentalTime::class)
 internal object InstantConverter : RoomTypeConverter<Long, Instant> {
   @TypeConverter
   override fun toEntity(value: Long): Instant {
