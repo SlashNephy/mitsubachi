1b44e14198e8932733f480d7cea3fcfe0a53c0f6
SlashNephy <spica@starry.blue>
2025-11-26 10:15:32 +0900

    MitsubachiDatabase にリネーム

--- core/data/src/main/java/blue/starry/mitsubachi/core/data/repository/SecureSettingsRepositoryImpl.kt
+++ core/data/src/main/java/blue/starry/mitsubachi/core/data/repository/SecureSettingsRepositoryImpl.kt
@@ -1,11 +1,11 @@
 package blue.starry.mitsubachi.core.data.repository
 
-import blue.starry.mitsubachi.core.data.database.EncryptedAppDatabase
+import blue.starry.mitsubachi.core.data.database.dao.SettingsDao
 import blue.starry.mitsubachi.core.domain.usecase.SecureSettingsRepository
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
 internal class SecureSettingsRepositoryImpl @Inject constructor(
-  private val database: EncryptedAppDatabase,
+  private val dao: SettingsDao,
 ) : SecureSettingsRepository
