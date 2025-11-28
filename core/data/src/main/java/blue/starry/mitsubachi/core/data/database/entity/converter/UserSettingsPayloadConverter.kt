68ce611cbbe866b824c37473b06a29abce3e50f4
SlashNephy <spica@starry.blue>
2025-11-28 13:55:16 +0900

    ignoreUnknownKeys = true とする

--- core/data/src/main/java/blue/starry/mitsubachi/core/data/database/entity/converter/UserSettingsPayloadConverter.kt
+++ core/data/src/main/java/blue/starry/mitsubachi/core/data/database/entity/converter/UserSettingsPayloadConverter.kt
@@ -5,13 +5,17 @@ import blue.starry.mitsubachi.core.data.database.entity.UserSettings
 import kotlinx.serialization.json.Json
 
 internal object UserSettingsPayloadConverter : RoomTypeConverter<String, UserSettings.Payload> {
+  private val json = Json {
+    ignoreUnknownKeys = true
+  }
+
   @TypeConverter
   override fun toEntity(value: String): UserSettings.Payload {
-    return Json.decodeFromString<UserSettings.Payload>(value)
+    return json.decodeFromString<UserSettings.Payload>(value)
   }
 
   @TypeConverter
   override fun fromEntity(entity: UserSettings.Payload): String {
-    return Json.encodeToString(entity)
+    return json.encodeToString(entity)
   }
 }
