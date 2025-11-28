fb363f67f77df1aa6c6715c88c9830d12e8f2749
SlashNephy <spica@starry.blue>
2025-11-28 13:57:28 +0900

    可能なら Swarm 互換のホームタイムラインを取得する

--- core/domain/src/main/java/blue/starry/mitsubachi/core/domain/usecase/FetchFeedUseCase.kt
+++ core/domain/src/main/java/blue/starry/mitsubachi/core/domain/usecase/FetchFeedUseCase.kt
@@ -1,12 +1,32 @@
 package blue.starry.mitsubachi.core.domain.usecase
 
+import blue.starry.mitsubachi.core.domain.error.UnauthorizedError
 import blue.starry.mitsubachi.core.domain.model.CheckIn
+import kotlinx.coroutines.flow.first
 import javax.inject.Inject
 import javax.inject.Singleton
 
 @Singleton
-class FetchFeedUseCase @Inject constructor(val client: FoursquareApiClient) {
+class FetchFeedUseCase @Inject constructor(
+  private val foursquare: FoursquareApiClient,
+  private val swarm: SwarmApiClient,
+  private val foursquareAccountRepository: FoursquareAccountRepository,
+  private val userSettingsRepository: UserSettingsRepository,
+) {
   suspend operator fun invoke(): List<CheckIn> {
-    return client.getRecentCheckIns()
+    // TODO: 複数アカウント対応
+    val account = foursquareAccountRepository.list().firstOrNull() ?: throw UnauthorizedError()
+    val settings = userSettingsRepository.flow(account).first()
+
+    if (!settings.useSwarmCompatibilityMode || settings.swarmOAuthToken.isBlank()) {
+      return foursquare.getRecentCheckIns()
+    }
+
+    return swarm.getRecentActivities(
+      oauthToken = settings.swarmOAuthToken,
+      uniqueDevice = settings.uniqueDevice,
+      wsid = settings.wsid,
+      userAgent = settings.userAgent,
+    )
   }
 }
