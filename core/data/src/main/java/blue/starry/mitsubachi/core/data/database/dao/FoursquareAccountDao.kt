655bdfcf87558bc2863632e0d226e2588e98dfe5
SlashNephy <spica@starry.blue>
2025-11-29 16:05:22 +0900

    プライマリなアカウントを返す Flow

--- core/data/src/main/java/blue/starry/mitsubachi/core/data/database/dao/FoursquareAccountDao.kt
+++ core/data/src/main/java/blue/starry/mitsubachi/core/data/database/dao/FoursquareAccountDao.kt
@@ -5,11 +5,15 @@ import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import blue.starry.mitsubachi.core.data.database.entity.FoursquareAccount
+import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FoursquareAccountDao {
   @Query("SELECT * FROM `foursquare_accounts` WHERE `id` = :id")
   suspend fun getById(id: String): FoursquareAccount?
+
+  @get:Query("SELECT * FROM `foursquare_accounts` WHERE `is_primary` = true LIMIT 1")
+  val primary: Flow<FoursquareAccount?>
 
   @Query("SELECT * FROM `foursquare_accounts`")
   suspend fun list(): List<FoursquareAccount>
--- core/data/src/main/java/blue/starry/mitsubachi/core/data/repository/FoursquareAccountRepositoryImpl.kt
+++ core/data/src/main/java/blue/starry/mitsubachi/core/data/repository/FoursquareAccountRepositoryImpl.kt
@@ -5,13 +5,18 @@ import blue.starry.mitsubachi.core.data.database.entity.FoursquareAccount
 import blue.starry.mitsubachi.core.data.repository.model.toDomain
 import blue.starry.mitsubachi.core.data.repository.model.toEntity
 import blue.starry.mitsubachi.core.domain.usecase.FoursquareAccountRepository
+import kotlinx.coroutines.flow.Flow
+import kotlinx.coroutines.flow.map
 import javax.inject.Inject
 import javax.inject.Singleton
 import blue.starry.mitsubachi.core.domain.model.FoursquareAccount as DomainFoursquareAccount
 
 @Singleton
-class FoursquareAccountRepositoryImpl @Inject constructor(private val dao: FoursquareAccountDao) :
-  FoursquareAccountRepository {
+class FoursquareAccountRepositoryImpl @Inject constructor(
+  private val dao: FoursquareAccountDao,
+) : FoursquareAccountRepository {
+  override val primary: Flow<DomainFoursquareAccount?> = dao.primary.map { it?.toDomain() }
+
   override suspend fun list(): List<DomainFoursquareAccount> {
     return dao.list().map(FoursquareAccount::toDomain)
   }
