0607aaca714d9487d47c4c6108708343cb2b07d3
SlashNephy <spica@starry.blue>
2025-12-02 22:32:04 +0900

    CancellationException は再スロー

--- core/ui/compose/src/main/java/blue/starry/mitsubachi/core/ui/compose/error/result.kt
+++ core/ui/compose/src/main/java/blue/starry/mitsubachi/core/ui/compose/error/result.kt
@@ -1,14 +1,15 @@
 package blue.starry.mitsubachi.core.ui.compose.error
 
+import kotlinx.coroutines.CancellationException
+
 inline fun <T> Result<T>.onException(action: (exception: Exception) -> Unit): Result<T> {
-  exceptionOrNull()?.also { throwable ->
-    if (throwable !is Exception) {
-      // 非 Exception は回復不能とみなして再スロー
-      throw throwable
+  return when (val exception = exceptionOrNull()) {
+    null -> this
+    is CancellationException -> throw exception
+    !is Exception -> throw exception // 非 Exception は回復不能とみなして再スロー
+    else -> {
+      action(exception)
+      this
     }
-
-    action(throwable)
   }
-
-  return this
 }
