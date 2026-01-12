751f59e8a51e0bbb90521fd9ffeab7449c0786de
SlashNephy <spica@starry.blue>
2026-01-12 20:35:44 +0900

    toolchain オプションで JVM を指定する

--- build-logic/convention/src/main/kotlin/AndroidConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/AndroidConventionPlugin.kt
@@ -1,15 +1,15 @@
 import com.android.build.api.dsl.ApplicationExtension
 import com.android.build.api.dsl.LibraryExtension
-import org.gradle.api.JavaVersion
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.plugins.JavaPluginExtension
 import org.gradle.api.tasks.testing.Test
+import org.gradle.jvm.toolchain.JavaLanguageVersion
 import org.gradle.kotlin.dsl.dependencies
 import org.gradle.kotlin.dsl.findByType
 import org.gradle.kotlin.dsl.kotlin
 import org.gradle.kotlin.dsl.withType
-import org.jetbrains.kotlin.gradle.dsl.JvmTarget
-import org.jetbrains.kotlin.gradle.tasks.KotlinJvmCompile
+import org.jetbrains.kotlin.gradle.dsl.KotlinJvmProjectExtension
 
 sealed interface AndroidProjectType {
   enum class UiFramework {
@@ -53,10 +53,14 @@ open class AndroidBaseConventionPlugin(private val projectType: AndroidProjectTy
         }
       }
 
-      tasks.withType<KotlinJvmCompile>().configureEach {
-        compilerOptions {
-          jvmTarget.set(JvmTarget.JVM_21)
+      val jvmVersion = 21
+      extensions.findByType<JavaPluginExtension>()?.apply {
+        toolchain {
+          languageVersion.set(JavaLanguageVersion.of(jvmVersion))
         }
+      }
+      extensions.findByType<KotlinJvmProjectExtension>()?.apply {
+        jvmToolchain(jvmVersion)
       }
 
       listOfNotNull(
@@ -85,11 +89,6 @@ open class AndroidBaseConventionPlugin(private val projectType: AndroidProjectTy
 
           buildFeatures {
             compose = composePluginEnabled
-          }
-
-          compileOptions {
-            sourceCompatibility = JavaVersion.VERSION_21
-            targetCompatibility = JavaVersion.VERSION_21
           }
 
           lint {
